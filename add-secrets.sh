b5003f23918b2649f65dd24f298035f4cc090c92
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-06-05 19:07:03 -0400

    demo code

---
+++ add-secrets.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+vault kv put  -mount=secret  my-app/database        user=admin    password=s3cr3t
+vault kv put  -mount=secret  my-app/api-keys        key1=abc123   key2=xyz123
+vault kv put  -mount=secret  my-app/foo/bar/nested  value=demo  
---
+++ demo-app2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo
+echo "==== DEMO APP 2 ===="
+echo "DATABASE_USER      = ${DATABASE_USER}"
+echo "DATABASE_PASSWORD  = ${DATABASE_PASSWORD}"
+echo "API_KEYS_KEY1      = ${API_KEYS_KEY1}"
+echo "API_KEYS_KEY2      = ${API_KEYS_KEY2}"
+echo "NESTED_VALUE       = ${NESTED_VALUE}"
+
+sleep 100
