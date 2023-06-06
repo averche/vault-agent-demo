5ca30869b0aa907b0fd4d3ed95c2f1d7dfce6cb5
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-06-06 15:47:52 -0400

    add secret

--- add-secrets.sh
+++ add-secrets.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+vault kv put  -mount=secret  test                   user=hello    password=vault
+
+# nested under my-app/
 vault kv put  -mount=secret  my-app/database        user=admin    password=s3cr3t
 vault kv put  -mount=secret  my-app/api-keys        key1=abc123   key2=xyz123
 vault kv put  -mount=secret  my-app/foo/bar/nested  value=demo  
